2af6a3be057fefedd010e8ddfa0c9860460d72d6
baronvonpurr <robinhood.svensson@hotmail.com>
2023-10-18 10:27:57 +0200

    Add project files.

---
+++ Inlämnings Uppgift4.1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Uppgift4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal = 10;
+            while (tal >= 10 && tal <=30)
+            {
+                Console.WriteLine(tal);
+                tal++;
+            }
+
+            tal = 200;
+            while (tal <= 200 && tal >= 180)
+            {
+                Console.WriteLine(tal);
+                tal--;
+            }
+
+            tal = 1000;
+            while (tal >= 1000 && tal <= 1400)
+            {
+                Console.WriteLine(tal);
+                tal = tal + 50;
+            }
+            Console.ReadKey();
+        }
+    }
+}
